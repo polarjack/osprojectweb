de6602fcd508389a6414fb5237dea7c756df5b9f
JACK <luckyjack85@gmail.com>
2018-01-02 16:37:51 +0800

    init

---
+++ items.js
@@ -0,0 +1,28 @@
+var user_id = [
+  "user0",
+  "user1",
+  "user2",
+  "user3",
+  "user4",
+  "user5",
+  "user6",
+  "user7",
+  "user8",
+  "user9"
+]
+
+var elevatorposition = 10 + 9 * 80;
+
+
+var peoplepositionX = [
+  60, 60, 60, 60, 60, 60, 60, 60, 60, 60
+]
+
+// var peoplepositionY = [
+//   , 0, 0, 0, 0, 0, 0, 0, 0, 0
+// ]
+
+
+var floors = [750, 670, 590, 510, 430, 350, 270, 190, 110, 30]
+
+var passenger = []
---
+++ onload.js
@@ -0,0 +1,94 @@
+function onload() {
+  console.log("onload")
+  
+  // setelevatorposition()
+  // peopleshow(0, 0)
+  // peoplegetinelevator(0)
+  // elevatorup()
+  // elevatorup()
+  
+}
+
+
+function getObject(id) {
+  return $('#' + id)
+}
+
+function setelevatorposition() {
+  TweenLite.to("#elevator", 0.1, {
+    marginTop: elevatorposition + 'px'
+  })
+  TweenLite.to("#elevator", 0.5, {
+    opacity: 1,
+    delay: 0.1
+  })
+}
+
+function elevatordown() {
+  elevatorposition += 82;
+
+  TweenLite.to("#elevator", 0.5, {
+    marginTop: elevatorposition + 'px'
+  })
+  passenger.map(i => {
+    TweenLite.to("#people" + i, 0.5, {
+      marginTop: elevatorposition + 'px'
+    })
+  })
+}
+
+function elevatorup() {
+  elevatorposition -= 80;
+  
+  TweenLite.to("#elevator", 0.5, {
+    marginTop: elevatorposition + 'px'
+  })
+  passenger.map(i => {
+    TweenLite.to("#people" + i, 0.5, {
+      marginTop: elevatorposition + 'px'
+    })
+  })
+}
+
+
+// function peopledown(id) {
+//   peopleposition[id]
+//   TweenLite.to("#"+ id, 0.5, {
+//     marginTop: peopleposition[id] + 'px'
+//   })
+// }
+
+function initpeopleposition() {
+  floors.map((v, i) => {
+    peopleposition[i] = v
+  });
+}
+
+function peopleshow(id, atfloor) {
+
+  TweenLite.to("#people" + id, 0.1, {
+    marginTop: floors[atfloor] + 'px'
+  })
+  TweenLite.to("#people" + id, 0.4, {
+    opacity: 1,
+    delay: 0.1
+  })
+}
+
+function peoplegetinelevator(id) {
+  peoplepositionX[id] += 70
+  passenger.push(id)
+  TweenLite.to("#people" + id, 0.5, {
+    marginLeft: peoplepositionX[id] + 'px'
+  })
+}
+
+function peoplegetoutelevator(id) {
+  peoplepositionX[id] += 40
+  TweenLite.to("#people" + id, 0.5, {
+    marginLeft: peoplepositionX[id] + 'px'
+  })
+  TweenLite.to("#people" + id, 0.1, {
+    opacity: 0
+  })
+}
